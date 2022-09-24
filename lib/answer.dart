60c201eddd1e3d23e521ecd19d048bf0137bd13b
Berkay KIZGIR <berkaykizgir@outlook.com>
2022-09-25 00:19:32 +0300

    button style

--- lib/answer.dart
+++ lib/answer.dart
@@ -11,10 +11,15 @@ class Answer extends StatelessWidget {
     return Container(
         width: double.infinity,
         margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        decoration: BoxDecoration(
+          gradient: const LinearGradient(colors: [Colors.cyan, Colors.indigo]),
+          borderRadius: BorderRadius.circular(20),
+        ),
         child: ElevatedButton(
           onPressed: selectHandler,
           style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(Colors.blue)),
+              backgroundColor: MaterialStateProperty.all(Colors.transparent),
+              shadowColor: MaterialStateProperty.all(Colors.transparent)),
           child: Text(
             answerText,
             style: const TextStyle(color: Colors.white),
